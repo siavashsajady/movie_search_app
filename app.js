25a70897988ed55ebb8ee5daa0ef983eac8df305
Siavash <siavash.sajjady@gmail.com>
2019-02-28 10:48:04 +0000

    Add To Github

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+
+app.set("view engine" , "ejs");
+
+app.get("/", function(req, res) {
+    res.render("search");
+});
+
+
+//result
+app.get("/results", function(req,res){
+    var query = req.query.search;
+    var url = "https://www.omdbapi.com/?s=";
+    var key = "&apikey=thewdb";
+
+    request(url + query + key, function(error,response,body){
+       if(!error && response.statusCode ==200){
+           var data = JSON.parse(body);
+               //res.send(results["Search"][0]["Title"]);
+               res.render("results", {data: data});
+       }
+   });
+});
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("Movie App has Started!!!") ;
+});
